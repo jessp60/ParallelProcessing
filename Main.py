fa88b7c3e500f39f329d35f786d4aa75e7b62575
jessp60 <jmpinto@cpp.edu>
2025-11-03 14:21:41 -0800

    added minimal UI and timer

--- Main.py
+++ Main.py
@@ -2,11 +2,7 @@
 from requests_html import HTML, HTMLSession 
 from thread import thread
 from concurrent.futures import ThreadPoolExecutor, as_completed
-
-# pass contents of file into HTML class 
-# with open('simple.html') as html_file: 
-#     source = html_file.read()
-#     html = HTML(html=source) # instance of HTML object 
+import time
 
 session = HTMLSession()
 
@@ -16,37 +12,43 @@ def scrape_page(title):
         response = session.get(url)
         threadOverview = response.html.find("h2") #get all the <h2> elements
         if threadOverview:
-            result = [f"\npage title: {title}", "items on page: "]
-            for overview in threadOverview:
-                result.append(f"- {overview.text}")
+            print(f"\nPage title: {title}")
+            result = [f"\nPage title: {title}"]
+            threadOverview = response.html.find("div.contentsPage__intro p", first = True)
+            if threadOverview: 
+                print("Description:", threadOverview.text)
             return "\n".join(result)
         else:
-            return f"\npage title: {title}\nNo items found on page."
+            return f"\nPage title: {title}\nNo items found on page."
     except Exception as e:
-        return f"\npage title: {title}\nError occurred: {e}"
+        return f"\nPage title: {title}\nError occurred: {e}"
 
 def main():
     r = session.get('https://en.wikipedia.org/wiki/Wikipedia:Contents') # response object 
     titles = r.html.find('h3')
-# threadNum = 0 
-    subset = [titles[i].text for i in range(0, 13)]
+    # threadNum = 0 
+    titles = [titles[i].text for i in range(0, 13)]
     
-    with ThreadPoolExecutor(max_workers=5) as executor:
-        futures = [executor.submit(scrape_page, title) for title in subset]
+    multiP = input("Please select the scraping method (Baseline, Forking, MultiThreading): ")
+    startTime = time.perf_counter() 
 
-        for future in as_completed(futures):
-            print(future.result())
+    match multiP: 
+        case "Baseline": 
+            print("Baseline Threading")
+            for title in titles:
+                scrape_page(title)
+        case "MultiThreading": 
+            # Scrape page using threads 
+            with ThreadPoolExecutor(max_workers=13) as executor:
+                [executor.submit(scrape_page, title) for title in titles]
+        case "Forking": 
+        # Scrape page using forks
+            pass
+        case _: 
+            print("Not a valid choice!")
+    endTime = time.perf_counter() 
+    
+    print(f"\nTotal {multiP} Processing Time: ", round(endTime-startTime, 3))
 
 if __name__ == "__main__":
     main()
-
-      # Browse by subject
-# for i in range(0, 13):  # Browse by subject 
-#     pageName = titles[i].text
-#     print(f"\nPage Title: {pageName}")
-#     thread = session.get(f"https://en.wikipedia.org/wiki/Wikipedia:Contents/{pageName}")
-#     threadOverview = thread.html.find("h2")
-#     if threadOverview: 
-#         print("Items on page:")
-#         for overview in threadOverview: 
-#             print("-", overview.text) 
