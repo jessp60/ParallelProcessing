a6f54d6aae4631ae26e1a1a70f32c399cda48cfe
jessp60 <jmpinto@cpp.edu>
2025-11-03 13:40:12 -0800

    updates for ui

--- Main.py
+++ Main.py
@@ -14,12 +14,22 @@ r = session.get('https://en.wikipedia.org/wiki/Wikipedia:Contents') # response o
 titles = r.html.find('h3')
 threadNum = 0 
 
-for i in range(0, 13):  # Browse by subject 
+# for i in range(0, 13):  # Browse by subject
+#     pageName = titles[i].text
+#     print(f"\nPage Title: {pageName}")
+#     thread = session.get(f"https://en.wikipedia.org/wiki/Wikipedia:Contents/{pageName}")
+#     threadOverview = thread.html.find("h2")
+#     if threadOverview: 
+#         print("Items on page:")
+#         for overview in threadOverview: 
+#             print("-", overview.text) 
+
+for i in range(0, 13):  # Browse by subject
     pageName = titles[i].text
-    print(f"\nPage Title: {pageName}")
+    print(f"\nPage: {pageName}")
     thread = session.get(f"https://en.wikipedia.org/wiki/Wikipedia:Contents/{pageName}")
-    threadOverview = thread.html.find("h2")
+    threadOverview = thread.html.find("div.contentsPage__intro p", first = True)
     if threadOverview: 
-        print("Items on page:")
-        for overview in threadOverview: 
-            print("-", overview.text) 
+        print("Description:", threadOverview.text)
+
+# will have to strip the text to include spaces 
